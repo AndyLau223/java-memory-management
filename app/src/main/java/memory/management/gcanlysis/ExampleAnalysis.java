61bab6168b7373304ecd0380b04fb06af3b964d3
Andy <jiancong.liu@outlook.com>
2023-05-14 22:27:02 +0800

    add comments.

--- app/src/main/java/memory/management/gcanlysis/ExampleAnalysis.java
+++ app/src/main/java/memory/management/gcanlysis/ExampleAnalysis.java
@@ -3,6 +3,14 @@ package memory.management.gcanlysis;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This sample code is for the GC analysis.
+ * Using VisualVM to grab the JVM metrics.
+ *
+ * You can launch the application with different heap size to
+ * experiment what will happen when heap size running out of space  and
+ * what will the GC activity looks like.
+ */
 public class ExampleAnalysis {
 
     public static List<String> stringList = new ArrayList<>();
